ecdc9a1acc09293eb99ab6e351ff24dc179ca66a
Jun Kawasaki <04.feasts_minded@icloud.com>
2026-08-11 18:54:32 +0900

    chore(native): pin tail-position record validation

--- test/kotoba/compiler/aggregate_abi_test.clj
+++ test/kotoba/compiler/aggregate_abi_test.clj
@@ -8,7 +8,7 @@
   (get-in (edn/read-string (slurp "deps.edn")) [:deps coordinate :git/sha]))
 
 (deftest pinned-closure-carries-the-complete-native-boundary
-  (is (= "8b1e22c9fb645e38ce16c3f2e24fc10468eba14d"
+  (is (= "dd7fe0cf8972ef20bcc24894b7c7aaf9d086c1a6"
          (dependency-pin 'io.github.kotoba-lang/kotoba-native)))
   (is (= "d58972da61758584a5bec0e863bcb3ea6fdd6c64"
          (dependency-pin 'io.github.kotoba-lang/kotoba-kir)))
