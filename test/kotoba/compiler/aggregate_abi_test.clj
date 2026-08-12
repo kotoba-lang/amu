d1e5fc8fd04d2dd172b76bb4a685083cfd4022b9
Jun Kawasaki <jun784@gmail.com>
2026-08-12 12:58:08 +0900

    Pin AArch64 logical immediate selection

--- test/kotoba/compiler/aggregate_abi_test.clj
+++ test/kotoba/compiler/aggregate_abi_test.clj
@@ -8,7 +8,7 @@
   (get-in (edn/read-string (slurp "deps.edn")) [:deps coordinate :git/sha]))
 
 (deftest pinned-closure-carries-the-complete-native-boundary
-  (is (= "98dd22a63044ce9cd5ddb7babb3efc7cdcddcbbe"
+  (is (= "b11f869d06a955fd7a47d5ecdb800718df7c6419"
          (dependency-pin 'io.github.kotoba-lang/kotoba-native)))
   (is (= "d58972da61758584a5bec0e863bcb3ea6fdd6c64"
          (dependency-pin 'io.github.kotoba-lang/kotoba-kir)))
