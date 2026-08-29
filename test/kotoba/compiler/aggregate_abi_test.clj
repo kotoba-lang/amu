537442b0e2a6fa7550b946db8099d9062c10fcbe
Jun Kawasaki <jun784@gmail.com>
2026-08-29 13:29:10 +0900

    WIP: advance kotoba-native to 689757a (verification pending)

--- test/kotoba/compiler/aggregate_abi_test.clj
+++ test/kotoba/compiler/aggregate_abi_test.clj
@@ -8,7 +8,7 @@
   (get-in (edn/read-string (slurp "deps.edn")) [:deps coordinate :git/sha]))
 
 (deftest pinned-closure-carries-the-complete-native-boundary
-  (is (= "1fd9c22fb40da42c15ae1b099f7d277a65028dd6"
+  (is (= "689757abb6e68fbe29b1fa3fef6f8c278977ea86"
          (dependency-pin 'io.github.kotoba-lang/kotoba-native)))
   (is (= "099c627609ad506babf05f2d5e9a73b95c1b026b"
          (dependency-pin 'io.github.kotoba-lang/kotoba-kir)))
